8048e032273d711d5296edee07e5702d29153152
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-04-19 12:17:28 +0200

    scraping movies links

--- scraper.py
+++ scraper.py
@@ -6,8 +6,17 @@ response = requests.get("https://www.filmweb.pl/netflix")
 html_soup = BeautifulSoup(response.text, 'html.parser')
 movies = []
 movies = html_soup.find('section', {"id": "netflixMoviesOfTheMonth"})
-titles = []
-titles = movies.find_all('h3',{'class':'simplePoster__heading'})
-for title in titles:
-    title = title.text
-    print (title)
+mailing = [{'title': '','poster': '','rate': 0, 'description': "", 'review': "", "link": ''}]
+mailing.clear()
+# titles = movies.find_all('h3',{'class':'simplePoster__heading'})
+for movie in movies.find_all('a', class_ ='simplePoster__title'):
+    mailing.append({'title': '','poster': '','rate': 0, 'description': "", 'review': "", 'link': "https://www.filmweb.pl"+movie['href']})
+
+    print ("https://www.filmweb.pl"+movie['href'])
+
+
+# for title in movies.find_all('h3',{'class':'simplePoster__heading'}):
+#     mailing.append({'title': title.text,'poster': '','rate': 0, 'description': "", 'review': ""})
+
+
+
