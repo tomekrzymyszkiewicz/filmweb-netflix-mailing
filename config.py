53969b4d042f79a94920eb595d67706392ed1e4f
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-04-19 11:14:14 +0200

    try added

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import configparser
 
 def load_config(file_name):
-    config = configparser.ConfigParser()
-    config.read(file_name)
-    sender = {"mail": config['sender']['mail'], "password": config['sender']['password']}
-    receivers = []
-    for receiver in config['receivers'].values():
-        receivers.append(receiver)
-    return sender, receivers
+    try:
+        config = configparser.ConfigParser()
+        config.read(file_name)
+        sender = {"mail": config['sender']['mail'], "password": config['sender']['password']}
+        receivers = []
+        for receiver in config['receivers'].values():
+            receivers.append(receiver)
+        return sender, receivers
+    except:
+        print("Loading config error")
