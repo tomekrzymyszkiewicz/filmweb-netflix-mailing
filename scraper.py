dc1e4f01cd3a293d7677cf7a77e8706fc29600c8
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2021-04-19 23:57:07 +0200

    added sorting movies by rate

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ def get_films_of_month():
     response = requests.get("https://www.filmweb.pl/netflix")
     html_soup = BeautifulSoup(response.text, 'html.parser')
     movies_page = html_soup.find('section', {"id": "netflixMoviesOfTheMonth"})
-    movies = [{'title': '','poster': '','rate': 0, 'description': "", 'review': "", "link": ''}]
+    movies = [{'title': '','poster': '','rate': 0, 'description': "", 'review': "", "link": '', 'sort_rate': 0}]
     movies.clear()
     for movie in movies_page.find_all('a', class_ ='simplePoster__title'):
         movies.append({'title': '','poster': '','rate': 0, 'description': "", 'link': "https://www.filmweb.pl"+movie['href']})
@@ -19,6 +19,9 @@ def get_films_of_month():
         movie['title'] = html_soup.find('h1').span.text
         movie['poster'] = html_soup.find('img', id = 'filmPoster')['content']
         movie['rate'] = html_soup.find('span', class_ = 'filmRating__rateValue').text.strip() + " (" + html_soup.find('span', class_ = 'filmRating__count').    text.strip() + ")"
+        movie['sort_rate'] = html_soup.find('span', class_ = 'filmRating__rateValue').text.strip()
         movie['description'] = html_soup.find('span', itemprop = 'description').text
+
+    movies.sort(key=lambda item: item.get('sort_rate'),reverse=True)
 
     return movies
